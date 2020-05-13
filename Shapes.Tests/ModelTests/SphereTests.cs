7dcfb0581daaa470c638722a3c12cda739fe41ad
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-13 11:36:59 -0700

    Meaningful Surface Sphere Fail

--- Shapes.Tests/ModelTests/SphereTests.cs
+++ Shapes.Tests/ModelTests/SphereTests.cs
@@ -15,5 +15,14 @@ namespace Shapes.Tests
       double result = orb.GetVolume();
       Assert.AreEqual(result,523.6);
     }
+
+    [TestMethod]
+    public void Sphere_GetSurface_SurfaceAreaOfSphere()
+    {
+      Circle disk = new Circle(5.0);
+      Sphere orb = new Sphere(disk);
+      double result = orb.GetSurface();
+      Assert.AreEqual(result,314.16);
+    }
   }
 }
--- Shapes/Models/Sphere.cs
+++ Shapes/Models/Sphere.cs
@@ -15,5 +15,12 @@ namespace Shapes.Models
     {
      return Math.Round((4.0*Math.PI*Math.Pow(CrossSection.Radius, 3.0))/3.0, 1);
     }
+
+    public double GetSurface()
+    {
+      return 0; //return Math.Round(4*Math.PI* Math.Pow(CrossSection.Radius,2),2)
+    }
+
   }
+
 }
